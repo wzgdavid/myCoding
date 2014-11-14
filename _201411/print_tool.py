c853422f7fdceb9d0e5d230df1d978fc4c023c43
wzgdavid <wzgdavid@sina.com>
2014-11-14 11:19:33 +0800

    add print tool

---
+++ _201411/print_tool.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+
+import json
+
+uid = '56100215267'
+qz_path = '/alidata/sites/stg/qz/'
+#qz_path = '/data/sites/stg/qz/'
+
+
+def pprint(obj):
+    '''清晰显示dict'''
+    class MyEncoder (json.JSONEncoder):
+        def default(self, o):
+            try:
+                iterable = iter(o)
+            except TypeError:
+                pass
+            else:
+                return list(iterable)
+            try:
+                return json.JSONEncoder.default(self, o)
+            except TypeError:
+                return str(o)
+    
+    orig = json.dumps(obj,
+                      indent=4,
+                      sort_keys=True,
+                      skipkeys=True,
+                      cls=MyEncoder)
+    print eval("u'''%s'''" % orig).encode('utf-8')
+
+#d = 1,{'one': 1, 'two': False, 'three': {'one': 1, 'two': 2, 'three': 3}}
+#pprint(d)
