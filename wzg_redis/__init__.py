d88d4f619d001c566bb17a5d492f26efe661e6b7
wzgdavid <wzgdavid@sina.com>
2015-05-08 14:43:55 +0800

    redis ORM
    redis 中的数据与 model 对应

---
+++ wzg_redis/__init__.py
@@ -0,0 +1,137 @@
+import redis
+import json
+
+pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+r = redis.Redis(connection_pool=pool)
+
+
+person = {
+    'pk': 2,
+    'name': 'aaa',
+    'age': 66,
+}
+
+person = json.dumps(person)
+#r.set('person1', person)
+#pipe = r.pipeline()
+#pipe.set('foo', 'pipe')
+#print pipe.get('foo')
+#pipe.execute()
+#print r.get('foo')
+
+
+#print r.exists('foo')
+
+
+
+
+class Redis(object):
+    def __init__(self):
+        pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+        self.r = redis.Redis(connection_pool=pool)
+
+    def get(self, pk):
+        return self.r.get(pk)
+
+    def set(self, pk, value):
+        self.r.set(pk, value)
+
+
+redis_app = Redis()
+
+
+class Model(object):
+    @classmethod
+    def get(cls, pk):
+        all_info = redis_app.get(cls.__name__.lower() + str(pk))
+        #obj = type.__new__(cls, )
+        if not all_info:
+            return None
+        obj = cls()
+
+        all_info = json.loads(all_info)
+        for k, v in all_info.items():
+            obj.__setattr__(k, v)
+        
+        return obj
+
+    def test(self):
+        print 'model test'
+        #if self.r.exists(self.__whole_pk(self.pk)):
+        #    return
+        #self.r.set(self.__whole_pk(self.pk))
+
+
+    def save(self):
+        fields = self.__class__.fields
+        data = {}
+        for key in fields:
+            data[key] = getattr(self, key)
+
+        
+        #print data
+        json_data = json.dumps(data)
+
+        print 'json_data:', json_data
+        redis_app.set(self.__whole_pk(self.pk), json_data)
+
+    def delete(self, pk):
+        pass
+
+    def __whole_pk(self, pk):
+        return self.__class__.__name__.lower() + str(pk)
+
+
+class Person(Model):
+
+    fields = ['pk', 'name', 'age', 'info']
+
+    def __init__(self):
+        super(Person, self).__init__()  
+
+    @classmethod
+    def get(cls, pk):
+        obj = super(Person, cls).get(pk)
+        #print obj ,'has obj or not'
+        if not obj:
+            #print 'create obj'
+            cls.create(pk)
+        return obj
+
+    @classmethod
+    def create(cls, pk):
+        p = cls()
+        p.pk = pk
+        p.name = 'init'
+        p.age = 0
+        p.info = {
+            'a': 1,
+            'b': 2,
+            'c': 'cccc',
+        }
+        p.save()
+
+    def change_name(self, name):
+        self.name = name
+        self.save()
+
+
+
+
+#m.save()
+#m.save()
+#p = Person.get_instance('6')
+#print p
+
+if __name__ == '__main__':
+    # test redis
+    #redis_app.set('model1', 'test model 1')
+    #print redis_app.get('model1')
+
+    #m = Model.get(1)
+    #print m.test()
+
+    p = Person.get(5)
+    print p.name
+    #p.change_name('cccccc')
+
---
+++ wzg_redis/learn.py
@@ -0,0 +1,11 @@
+from __init__ import r
+from prettyprint import pp
+
+#print pp(r.info())
+
+#r.move('1',1)
+
+
+
+
+print r.keys()
