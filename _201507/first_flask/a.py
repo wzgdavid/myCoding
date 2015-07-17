af9e66d6b064078316c28c038fe6718fa6b3572c
wzgdavid <wzgdavid@sina.com>
2015-07-17 11:41:29 +0800

    first learn flask
    first learn flask

---
+++ _201507/first_flask/a.py
@@ -0,0 +1,78 @@
+from flask import Flask, request, session, g, make_response, redirect, url_for, abort, render_template, send_file, \
+    flash, json, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print('login post')
+    else:
+        print('login get')
+    return ' login ?'
+
+
+@app.route('/template/')
+@app.route('/template/<name>')
+def template(name=None):
+    return render_template('hello.html', name=name)
+
+
+@app.route('/returnjson')
+def returnjson():
+    return json.dumps({'name':'wzg', 'age':30})
+
+
+@app.route('/test')
+def test():
+    '''
+    template and dict
+    '''
+    data={'name':'wzg', 'age':30}
+    return render_template('hello.html', data=data)
+
+
+@app.route('/test2')
+def test2():
+    '''
+    template and object
+    '''
+    class Person(object):
+        """docstring for Person"""
+        def __init__(self, name, age):
+            super(Person, self).__init__()
+            self.name = name
+            self.age = age
+            
+    person = Person('jimi', 300)
+    return render_template('hello.html', person=person)
+
+
+@app.route('/test3')
+def test3():
+    '''
+    list and object
+    '''
+    class Person(object):
+        """docstring for Person"""
+        def __init__(self, name, age):
+            super(Person, self).__init__()
+            self.name = name
+            self.age = age
+            
+    persons = [Person('jimi', 300), Person('tom', 400)]
+    return render_template('hello.html', persons=persons)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
