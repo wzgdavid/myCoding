fe4aff561ba5391b3b26d5b016e23e6b5b5ff21e
davidwei <david.wei@ringierchina.com>
2015-09-25 17:59:10 +0800

    hexun qihuo use class

--- _201509/hexunqihuo.py
+++ _201509/hexunqihuo.py
@@ -4,80 +4,68 @@ reload(sys)
 sys.setdefaultencoding("utf8")
 import urllib, urllib2
 from pprint import pprint
-from collections import Counter
+from collections import Counter, OrderedDict
 from bs4 import BeautifulSoup
+from jinja2 import Environment, PackageLoader
 
-all_href = []
-def get_max_index(soup):
-    listdh = soup.find('div', class_='listdh')
-    s1 = str(listdh).split('maxPage = ')[1]
-    s2 = s1.split(';')[0]
-    return int(s2)
-'''
-with open(filename) as f:
-   input = f.read()
-output = do_something(input)
-with open(filename, 'w') as f:
-   f.write(output)
-'''
-def fetch_data(qtype, qname):
-    '''
-	                           qtype 
-	# http://futures.hexun.com/industrynews/index.html') # 金属
-    # http://futures.hexun.com/agriculturenews/index.html') # 农副资讯 
-    # http://futures.hexun.com/nyzx/index.html  # 能源资讯
-	'''
-    global all_href
-    #f = urllib.urlopen('http://futures.hexun.com/industrynews/index.html') # 金属
-    #f = urllib.urlopen('http://futures.hexun.com/agriculturenews/index.html') # 农副资讯 
-    # http://futures.hexun.com/nyzx/index.html  # 能源资讯
-    f = urllib.urlopen('http://futures.hexun.com/%s/index.html' % qtype) #
-    soup = BeautifulSoup(f.read(), 'html.parser')
-    keywd = qname.decode('utf-8')
-    temp01 = soup.find('div', class_='temp01')
-    content = temp01.select('li > a')
-    hrefs = [a['href'] +' '+ a.string for a in content if keywd in a.string]
-    all_href.extend(hrefs)
-    #if hrefs:
-    #	print 'index'
-    #    pprint(hrefs)
-    max_idx = get_max_index(soup)
-    do_range = range(1685, max_idx)[::-1]
-    for idx in do_range:
-        print idx
-        f = urllib.urlopen('http://futures.hexun.com/%s/index-%s.html' % (qtype, idx))
+
+class HexunQihuo(object):
+    def __init__(self):
+        self.all_href = []
+
+    def __get_max_index(self, soup):
+        listdh = soup.find('div', class_='listdh')
+        s1 = str(listdh).split('maxPage = ')[1]
+        s2 = s1.split(';')[0]
+        return int(s2)
+
+    def fetch_data(self, qtype, qname):
+        '''
+          qtype is in [industrynews, agriculturenews, nyzx] 
+        # http://futures.hexun.com/industrynews/index.html') # 金属
+        # http://futures.hexun.com/agriculturenews/index.html') # 农副资讯 
+        # http://futures.hexun.com/nyzx/index.html  # 能源资讯
+        '''
+        self.qtype = qtype
+        self.qname = qname
+        f = urllib.urlopen('http://futures.hexun.com/%s/index.html' % qtype) #
         soup = BeautifulSoup(f.read(), 'html.parser')
+        keywd = qname.decode('utf-8')
         temp01 = soup.find('div', class_='temp01')
         content = temp01.select('li > a')
-        #print(content.prettify())
-        #hrefs = [a['href'] for a in content if keywd in a.string]
         hrefs = [a['href'] +' '+ a.string for a in content if keywd in a.string]
-        #links = {a['href']: a.string for a in content if keywd in a.string}
-        #pprint(links)
-        #links = []
-        #for a in content:
-        #    if keywd in a.string:
-        #        d = {'url': a['href'], 'title': a.string}
-        #        links.append(d)
-        #if hrefs:
-            #pprint(hrefs)
-        all_href.extend(hrefs)
-    #pprint(all_href)
-    all_hrefstr = '\n'.join(all_href)
+        self.all_href.extend(hrefs)
+        max_idx = self.__get_max_index(soup)
 
-    
-    #with open('file.txt', 'w') as ff:
-    #    ff.write(str(all_hrefstr))
-    return all_hrefstr
+        do_range = range(670, max_idx)[::-1]
+        for idx in do_range:
+            print idx
+            f = urllib.urlopen('http://futures.hexun.com/%s/index-%s.html' % (qtype, idx))
+            soup = BeautifulSoup(f.read(), 'html.parser')
+            temp01 = soup.find('div', class_='temp01')
+            content = temp01.select('li > a')
+            hrefs = [a['href'] +' '+ a.string for a in content if keywd in a.string]
+            self.all_href.extend(hrefs)
+        self.all_hrefstr = '\n'.join(self.all_href)
+        return self
+
+    def write_txt(self):
+        with open('file.txt', 'w') as ff:
+            ff.write(str(self.all_hrefstr))
+
+    def write_html(self):
+        env = Environment(loader=PackageLoader('hexunqihuo', 'templates'))
+        data = OrderedDict()
+        for href in self.all_href:
+            item = href.split(' ')
+            data[item[0]] = item[1]
+        template = env.get_template('template.html') # templates/template.html
+        html = template.render(data=data)
+        htmlfile = 'files/%s.html' % self.qname
+        with open(htmlfile, 'w') as ff:
+            ff.write(html)
 
 
-def write_txt(data):
-    with open('file.txt', 'w') as ff:
-        ff.write(str(data))
+if __name__ == '__main__':
 
-data = fetch_data('agriculturenews', u'豆粕')
-write_txt(data)
-#class Hexun():
-#	def __init__(self):
-#		self.f = 
-#		self.soup
+    HexunQihuo().fetch_data('industrynews', u'螺纹钢').write_html()
